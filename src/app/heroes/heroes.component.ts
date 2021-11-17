fafc374474bb8a5c26f6a660e1600d62683b5787
llccss1 <quintana.lucas@gmail.com>
2021-11-17 18:09:03 -0300

    intro angular

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  hero = 'Windstorm';
+  frutas = ['banana','pera','limon'];
+  numero = 1000;
+  persona = {nombre: 'Lucas', apellido: 'Quintana'};
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  obtenerFrutas() {}
+}
